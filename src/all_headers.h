2ae21c6dc392007fba5e8e5f72b47a0eede2ecdd
iPAS <ptiwatthanont@gmail.com>
2021-05-08 14:03:35 +0700

    Start stubs.

---
+++ src/all_headers.h
@@ -0,0 +1,12 @@
+#ifndef __ALL_HEADERS_H__
+#define __ALL_HEADERS_H__
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "bind.h"
+
+
+#endif  // __ALL_HEADERS__
---
+++ src/bind.h
@@ -0,0 +1,8 @@
+#ifndef __BIND_H__
+#define __BIND_H__
+
+
+
+
+
+#endif  // __BIND_H__
