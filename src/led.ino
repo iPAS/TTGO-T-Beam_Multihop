69bc99097a89ae398b6c2a37b9fd87802673183d
iPAS <ptiwatthanont@gmail.com>
2021-05-16 14:28:19 +0700

    Set LED active logic corresponding to the version.

--- src/led.ino
+++ src/led.ino
@@ -6,14 +6,15 @@
 
 #define LED_IO_V07 14
 #define LED_IO_V10 4
-static uint8_t led_io;
+static uint8_t led_io, led_active;
 
 
 // ----------------------------------------------------------------------------
 void led_setup(bool is_tbeam_version_less_v1) {
     led_io = (is_tbeam_version_less_v1)? LED_IO_V07 : LED_IO_V10;
+    led_active = (is_tbeam_version_less_v1)? HIGH : LOW;
     pinMode(led_io, OUTPUT);
-    digitalWrite(led_io, LOW);
+    digitalWrite(led_io, !led_active);
 }
 
 
@@ -24,13 +25,13 @@ void led_blinking_process() {
     if (millis() > next) {
         switch (state) {
         case 0:
-            digitalWrite(led_io, HIGH);
+            digitalWrite(led_io, led_active);
             next = millis() + BLINK_ON_PERIOD;
             state = 1;
             break;
 
         case 1:
-            digitalWrite(led_io, LOW);
+            digitalWrite(led_io, !led_active);
             next = millis() + BLINK_OFF_PERIOD;
             state = 0;
             break;
