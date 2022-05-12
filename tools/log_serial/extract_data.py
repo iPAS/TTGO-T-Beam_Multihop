60feb117c0dd8119c6ca163a7675ff307e3de3f8
iPAS <ptiwatthanont@gmail.com>
2022-05-12 21:53:12 +0700

    doctest

--- tools/log_serial/extract_data.py
+++ tools/log_serial/extract_data.py
@@ -8,7 +8,7 @@ import csv
 
 
 parser = argparse.ArgumentParser(description='Extract data from log file')
-parser.add_argument('log_dir', type=str)
+parser.add_argument('log_dir', type=str, help='Set the directory of log files')
 args = parser.parse_args()
 
 
@@ -80,8 +80,37 @@ def name_stations(data, stations):
 def extract_response(resp):
     """Extract the response message to be sent to server.
 
+    >>> extract_response('[2022-05-02 12:27:54.807055 0.001959] 1000 i2c interface on ok')
     >>> extract_response('[2022-05-02 11:35:33.812434 0.003139] 1000 i2c 27.7,65.0')
+    {'1': 27.7, '2': 65.0}
 
+    >>> extract_response('[2022-05-02 12:58:27.046442 0.002686] 1000 wind set ok')
+    >>> extract_response('[2022-05-02 12:38:37.413748 0.001749] 1000 wind 301,9.7')
+    {'4': 301, '5': 9.7}
+
+    >>> extract_response('[2022-05-02 12:38:28.791464 0.003035] 1000 rain set ok')
+    >>> extract_response('[2022-05-02 12:38:28.788429 0.003645] 22: $ rain set 0 0.2')
+    >>> extract_response('[2022-05-02 12:44:21.620966 0.002682] 1000 rain 3.1')
+    {'6': 3.1}
+
+    >>> extract_response('[2022-05-02 12:38:53.709259 0.005014] 44: $ landsld site_name   048')
+    >>> extract_response('[2022-05-02 12:38:53.716303 0.007044] 1000 landsld set site name ok')
+    >>> extract_response('[2022-05-02 12:52:29.286857 0.000964] 1000 landsld 36.6 0 12.1 0')
+    {'7': 36.6}
+
+    >>> extract_response('[2022-05-02 12:27:49.115454 0.003755] 3: $ uc20 start')
+    >>> extract_response('[2022-05-02 12:27:49.117865 0.002411] 1000 uc20 start ok')
+    >>> extract_response('[2022-05-02 12:52:40.978333 0.005108] 1000 uc20 99,,,99')
+    {'8': 99}
+
+    >>> extract_response('[2022-05-02 12:46:56.581830 0.001369] 1000 charger 13.5,54,13.6,0.0,19.7')
+    {'10': 13.5}
+
+    >>> extract_response('[2022-05-02 12:44:33.530255 0.002398] 1000 atod 0.131')
+    {'16': 0.131}
+
+    >>> extract_response('[2022-05-02 13:22:27.527841 0.002508] 1000 atod 18.286')
+    {'17': 18.286}
     """
     m = re.search(r'\[.*?\] +[0-9]+ +(?P<cmd>[0-9a-z]+) +(?P<data>.+) *', resp)
     if m is None:
@@ -165,15 +194,11 @@ def extract_response(resp):
             io_no: a2d_data
         }
 
-    else:
-        return None
-
-    extracted = resp  # DEBUG:
-    return extracted
+    return None
 
 
 # -----------------------------------------------------------------------------
-def analyze_data_and_send_server(data):
+def extract_data_lines(data):
     pattern_data = re.compile(r'\[[0-9:\.\- ]+\] 1000 [0-9a-z]+ .*')
     pattern_station = re.compile(r'\[[0-9:\.\- ]+\] \[D\] .*')
     pattern_meta = re.compile(r'\[\s*(?P<date>[0-9-]+)\s+(?P<time>[0-9:]+).*\]' +
@@ -185,27 +210,30 @@ def analyze_data_and_send_server(data):
                               r'\s+' + r'#(?P<order>[0-9]+)' +
                               r'\s+' + r'\^(?P<hop>[0-9]+)' +
                               '')
-    infos = []
+    extracted_data = []
 
     for d in data:
-        ## Data packet only
+        ## Data of the packet only
         matches_data = pattern_data.findall(d)
-        if matches_data:
-            station_info = pattern_station.search(d).group(0)
-            meta = pattern_meta.search(station_info).groupdict()
 
-            extracted = {}
+        if matches_data:
+            open_line = pattern_station.search(d).group(0)
+            meta = pattern_meta.search(open_line).groupdict()
+
+            extracted_infos = {}
             for matched in matches_data:
                 filtered = extract_response(matched)
                 if filtered is not None:
-                    extracted.update(filtered)
+                    extracted_infos.update(filtered)
 
-            if extracted:
-                print(f'\n>>> {station_info} <<<')  # DEBUG:
-                print(meta)  # DEBUG:
-                print(extracted)
+            if extracted_infos:
+                # print(f'\n>>> {open_line} <<<')  # DEBUG:
+                # print(meta)  # DEBUG:
+                # print(extracted_infos)  # DEBUG:
+                extracted_infos.update(meta)
+                extracted_data.append(extracted_infos)
 
-            # break  # DEBUG:
+    return extracted_data
 
 
 # -----------------------------------------------------------------------------
@@ -221,4 +249,7 @@ if __name__ == '__main__':
 
     data = name_stations(data, stations)  # Name the stations
 
-    analyze_data_and_send_server(data)
+    data = extract_data_lines(data)
+
+    for d in data:
+        print(d, '\n')  # DEBUG:
