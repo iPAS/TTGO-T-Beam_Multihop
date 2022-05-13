efbef065865f167b3da9b3c66dcf5a63bb717183
iPAS <ptiwatthanont@gmail.com>
2022-05-13 11:25:14 +0700

    Change the table name

--- tools/log_serial/sqlcmd.py
+++ tools/log_serial/sqlcmd.py
@@ -1,7 +1,7 @@
 
 ## Create if not exists
 create_table_if_not_exists = '''
-CREATE TABLE IF NOT EXISTS response (
+CREATE TABLE IF NOT EXISTS response_table (
     response_id INTEGER PRIMARY KEY,
     hash TEXT UNIQUE NOT NULL,
     extracted_data TEXT NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS response (
 ## Insert if not exists
 # https://www.geeksforgeeks.org/python-mysql-insert-record-if-not-exists-in-table/
 insert_if_not_exists = '''
-INSERT OR IGNORE INTO response(hash, extracted_data, datetime_created)
+INSERT OR IGNORE INTO response_table(hash, extracted_data, datetime_created)
 VALUES(?, ?, strftime("%s", "now"))
 '''
 
@@ -21,6 +21,6 @@ VALUES(?, ?, strftime("%s", "now"))
 def get_row_count(conn):
     cur = conn.cursor()
     cur.execute('''
-    SELECT response_id FROM response
+    SELECT response_id FROM response_table
     ''')
     return len(cur.fetchall())
